ffcce80ca457e7709c04fa97944a85c44d582e74
Shashankh-Varma <vshashankh@gmail.com>
2025-06-18 17:29:43 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_SQzdxiTXS', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '515uk9m3neatf0e395h8smokjn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
